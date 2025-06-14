056df6f823d42f9eba06dead139e84288cacffc4
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2025-06-13 20:16:35 -0700

    dungeon nav mesh and enemy spawning

--- Assets/Scripts/DungeonGeneration/Doorway.cs
+++ Assets/Scripts/DungeonGeneration/Doorway.cs
@@ -7,4 +7,10 @@ public class Doorway : MonoBehaviour
     public DoorDirection direction;
     public bool isConnected = false;
     public Vector2Int offset;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        print("Collided with doorway");
+        GetComponentInParent<RoomProperties>().onDoorEntered(collider.gameObject);
+    }
 }
