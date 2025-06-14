693269d54ca0612bc66ef4c97f066eeed5386c1f
Bailey Miller <112430726+baileyEmiller@users.noreply.github.com>
2025-06-13 20:15:24 -0700

    idk anymore

---
+++ Assets/Scripts/Entity Scripts/burstEnemyNavigation.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class burstEnemyNavigation : baseEnemyNavigation
+{
+    public float burstCooldown = .1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
